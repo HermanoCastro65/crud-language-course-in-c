297bd5c382f0fbc5504c0951118b5f044e9b525f
Hermano Castro <hermanocastro18@gmail.com>
2023-10-05 21:16:26 -0300

    update invalid argument mensage

--- main.c
+++ main.c
@@ -5,14 +5,15 @@
 #include "app.h"
 
 int main(int argc, char* argv[]) {
-  if(argv[1])
-    if(strcmp(argv[1], "-test") != 0){
-      printf("\n\nINVALID ARGUMENT: %s \n\n", argv[1]);
+  if (argv[1])
+    if (strcmp(argv[1], "-test") != 0) {
+      printf("\n\nINVALID ARGUMENT: '%s' \ntry '-test' for test mode\n\n",
+             argv[1]);
       exit(1);
     }
 
   if (run_all_tests(argv[1])) exit(1);
-  
+
   main_menu();
 
   return 0;
